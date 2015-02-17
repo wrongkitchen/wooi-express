5a89ed5b17581eabd3e7523374d862436a913ac9
wrongkitchen <wkc.kenji@gmail.com>
2015-02-17 17:20:33 +0800

    redirect by html

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -6,10 +6,6 @@ module.exports = function (app) {
 	app.use('/', router);
 };
 
-router.get('/redirect', function (req, res, next) {
-	res.render('redirect');
-});
-
 router.get('/', function (req, res, next) {
 	if(req.user)
 		res.render('main');
