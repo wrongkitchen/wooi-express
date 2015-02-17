37bbd398d392c4883633e27387b24d4e77925e6c
wrongkitchen <wkc.kenji@gmail.com>
2015-02-17 17:33:12 +0800

    redirect page accept all method

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -6,6 +6,9 @@ module.exports = function (app) {
 	app.use('/', router);
 };
 
+router.all('/redirect', function (req, res, next) {
+	res.render('redirect');
+});
 router.get('/', function (req, res, next) {
 	if(req.user)
 		res.render('main');
